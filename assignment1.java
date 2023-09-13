a1c56d8479d1de5f3bd032697abd7d6cb808a442
Arun Kumar <Arun060325>
2023-09-13 20:38:56 +0530

    first commit

---
+++ assignment1.java
@@ -0,0 +1,20 @@
+
+public class assignment1
+{
+
+	public static void main(String[] args)
+	{
+		assignment1 G1 = new assignment1();
+		variabledemo3 j = new variabledemo3();
+		G1.funA(j);
+		G1.funA(new variabledemo3());
+		G1.funA(null);
+		 
+	}
+	void funA(variabledemo3 v3)
+	{
+		
+	}
+	
+
+}
